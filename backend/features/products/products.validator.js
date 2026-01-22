320504e931ad2e3711e62e3c1f65d7b6891fc71a
Khun Min Myat <minmyatz617@gmail.com>
2026-01-22 16:16:23 +0630

    POS part 2: Products - validator done

---
+++ backend/features/products/products.validator.js
@@ -0,0 +1,51 @@
+export const validator =
+  (isUpdate = false) =>
+  (req, res, next) => {
+    try {
+      const product = req.body;
+
+      // create
+      if (!isUpdate) {
+        // 1. product name
+        if (product.name === undefined)
+          throw new Error("Product is missing a name.");
+        // 2. product price
+        if (product.price === undefined) throw new Error("Product is missing ");
+        // 3. product quantity
+        if (product.stockQuantity === undefined)
+          throw new Error("Stock Quantity is missing.");
+      }
+
+      if (product.name !== undefined && typeof product.name !== "string")
+        throw new Error("Name must be a string.");
+      if (
+        product.price !== undefined &&
+        (typeof product.price !== "number" || product.price < 0)
+      )
+        throw new Error("Price must be a non-negative number.");
+      if (
+        product.stockQuantity !== undefined &&
+        (typeof product.stockQuantity !== "number" || product.stockQuantity < 0)
+      )
+        throw new Error("Stock Quantity must be a non-negative number.");
+
+      if (product.sku !== undefined && typeof product.sku !== "string")
+        throw new Error("Sku must be a string.");
+      if (
+        product.isActive !== undefined &&
+        typeof product.isActive !== "boolean"
+      )
+        throw new Error("isActive must be boolean.");
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+
+// {
+//   name: "Coke",
+//   price: 1000,
+//   stockQuantity: 5,
+//   sku: "COKE-001",    // optional
+//   isActive: true      // optional, mostly for update
+// }
