c3d4b569bdee4aa4d86e3d77d4f092e9175ec109
Khun Min Myat <minmyatz617@gmail.com>
2026-01-26 18:21:02 +0630

    added: auth route

--- backend/features/auth/auth.route.js
+++ backend/features/auth/auth.route.js
@@ -3,3 +3,9 @@ import { Router } from "express";
 import authMiddleware from "./auth.middleware";
 
 const router = Router();
+
+router.get("/register");
+router.get("/login");
+router.get("/refresh");
+
+export default router;
