64348fae9d10447e3a76302e76e086ff72b48354
Khun Min Myat <minmyatz617@gmail.com>
2026-04-20 16:43:31 +0900

    Convert: JS to TS

--- frontend/src/app/products/layout.jsx
+++
@@ -1,7 +0,0 @@
-export default function ProductLayout({children}) {
-    return (
-        <div className="min-h-screen flex items-center justiy-center bg-gray-50">
-            {children}
-        </div>
-    )
-}
